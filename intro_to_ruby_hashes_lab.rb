456306b1e9694824d3c336c8639dd4f7694eb644
Colton stephens <stephens.colton@icloud.com>
2020-02-25 19:24:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,12 @@
 def base_hash
-  
+   monopoly =  {
+    :railroads=>
+  {
+  }
+}
 end
 
 def monopoly_with_second_tier
- base = {
-	  :railroads => {}
-	    :pieces => 4
-
-	}
-   }
 end
 
 def monopoly_with_third_tier
